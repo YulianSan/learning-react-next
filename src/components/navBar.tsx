d0cf3e543b7a91a313a9c4d9b28771e41f6e8110
YulianSan <knoob0733@gmail.com>
2023-04-07 20:09:05 +0000

    Adicionando a funcionalidade de expandir o menu

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,32 +1,58 @@
+'use client'
 import Image from 'next/image';
 import style from '../style/components/navBar.module.css';
 import { NavItem } from './navItem';
+import { Dispatch, SetStateAction, useState } from 'react';
+
+const handleExpandMenu = (setExpand: Dispatch<SetStateAction<boolean>>)=>{
+    setExpand(prev => !prev);
+}
 
 export function NavBar(){
+    const [expand, setExpand] = useState(true);
+
     return(
-        <nav className={style.navbar}>
-            <Image alt="Icon Netflix" src={'/icon-netflix.png'} width={50} height={50} className={style.iconNavBar}/>
+        <nav className={`${style.navbar} ${expand && style.expand}`}>
+            <Image 
+                alt="Icon Netflix" 
+                src={'/icon-netflix.png'} 
+                width={50} 
+                height={50} 
+                className={style.iconNavBar}/>
+
             <ul>
                 <NavItem 
-                    href='/home' 
-                    src='/icon-favorite.png' 
-                    alt='favorite' 
-                    label='Favoritos'
+                    href='/' 
+                    src='/icon-home.png' 
+                    alt='Icon Home' 
+                    label='Home'
                     styleImg={{filter: 'invert(100%)'}}/>
+                
                 <NavItem 
                     href='/home' 
-                    src='/icon-favorite.png' 
-                    alt='favorite' 
-                    label='Favoritos'
+                    src='/icon-ver-depois.png' 
+                    alt='Ver depois' 
+                    label='Assitir mais tarde'
                     styleImg={{filter: 'invert(100%)'}}/>
+
                 <NavItem 
-                    href='/home' 
+                    href='/produto/1' 
                     src='/icon-favorite.png' 
                     alt='favorite' 
                     label='Favoritos'
                     styleImg={{filter: 'invert(100%)'}}/>
 
             </ul>
+
+            <Image 
+                alt="Expand menu" 
+                src={'/arrow-right.png'} 
+                width={50} 
+                height={50}
+                className={style.expandNavBar}
+                onClick={()=>
+                    handleExpandMenu(setExpand)
+                }/>
         </nav>
     )
 }
